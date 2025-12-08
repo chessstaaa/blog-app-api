04fe8e37e5cade4ebea5a2840928ebf0383bc20e
Calvin Halim <cavim1998@gmail.com>
2025-12-08 16:17:28 +0700

    add event API

---
+++ src/modules/event/event.controller.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { EventService } from "./event.service";
+import { GetEventsQuery } from "../../types/event";
+
+export class EventController {
+  eventService: EventService;
+
+  constructor() {
+    this.eventService = new EventService();
+  }
+
+  getEvents = async (req: Request, res: Response) => {
+    const query: GetEventsQuery = {
+      page: parseInt(req.query.page as string) || 1,
+      take: parseInt(req.query.take as string) || 3,
+      sortBy: (req.query.sortBy as string) || "createdAt",
+      sortOrder: (req.query.sortOrder as string) || "desc",
+      search: (req.query.search as string) || "",
+    };
+    const result = await this.eventService.getEvents(query);
+    return res.status(200).send(result);
+  };
+
+  getEventById = async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const result = await this.eventService.getEventById(id);
+    return res.status(200).send(result);
+  };
+}
