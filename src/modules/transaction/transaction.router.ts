ddfea47778e1e53cff5887e8078683d78dcce430
Calvin Halim <cavim1998@gmail.com>
2026-01-02 22:05:09 +0700

    adjust merge transaction

--- src/modules/transaction/transaction.router.ts
+++ src/modules/transaction/transaction.router.ts
@@ -1,51 +1,23 @@
-<<<<<<< HEAD
 import { Router } from "express";
 import { JwtMiddleware } from "../../middlewares/jwt.middleware";
+import { validateBody } from "../../middlewares/validation.middleware";
+import { uploader } from "../../middlewares/uploader.middleware";
+import { CreateTransactionDTO } from "./dto/create-transaction.dto";
 import { TransactionController } from "./transaction.controller";
 
 export class TransactionRouter {
-  router: Router;
-  transactionController: TransactionController;
-  jwtMiddleware: JwtMiddleware;
+  router = Router();
+  controller = new TransactionController();
+  jwt = new JwtMiddleware();
 
   constructor() {
-    this.router = Router();
-    this.transactionController = new TransactionController();
-    this.jwtMiddleware = new JwtMiddleware();
-    this.initRoutes();
-  }
-
-  private initRoutes = () => {
-    this.router.get("/", this.transactionController.getTransactions);
-    this.router.post("/approve", this.transactionController.acceptTransaction);
-    this.router.post("/reject", this.transactionController.rejectTransaction);
-  };
-
-  getRouter = () => {
-    return this.router;
-  };
-=======
-import { Router } from "express"
-import { JwtMiddleware } from "../../middlewares/jwt.middleware"
-import { validateBody } from "../../middlewares/validation.middleware"
-import { uploader } from "../../middlewares/uploader.middleware"
-import { CreateTransactionDTO } from "./dto/create-transaction.dto"
-import { TransactionController } from "./transaction.controller"
-
-export class TransactionRouter {
-  router = Router()
-  controller = new TransactionController()
-  jwt = new JwtMiddleware()
-
-  constructor() {
-
     // 🔥 CREATE TRANSACTION (CHECKOUT)
     this.router.post(
       "/",
       this.jwt.verifyToken(process.env.JWT_SECRET!),
       validateBody(CreateTransactionDTO),
       this.controller.createTransaction
-    )
+    );
 
     // upload bukti bayar
     this.router.post(
@@ -53,38 +25,38 @@ export class TransactionRouter {
       this.jwt.verifyToken(process.env.JWT_SECRET!),
       uploader.single("image"),
       this.controller.uploadProof
-    )
+    );
 
     // admin accept
     this.router.post(
       "/:id/accept",
       this.jwt.verifyToken(process.env.JWT_SECRET!),
       this.controller.accept
-    )
+    );
 
     // admin reject
     this.router.post(
       "/:id/reject",
       this.jwt.verifyToken(process.env.JWT_SECRET!),
       this.controller.reject
-    )
+    );
 
     this.router.get(
       "/pending",
       this.jwt.verifyToken(process.env.JWT_SECRET!),
       this.controller.getPending
-    )
+    );
 
     this.router.get(
       "/my",
       this.jwt.verifyToken(process.env.JWT_SECRET!),
       this.controller.getMyHistory
-    )
+    );
 
+    this.router.get("/dashboard", this.controller.getTransactions);
   }
 
   getRouter() {
-    return this.router
+    return this.router;
   }
->>>>>>> git-chesta
 }
